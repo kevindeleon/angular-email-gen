795b8c9df498c7a715a880a58f8198ab4f7a94d8
Kevin deLeon <kevin.deleon@gmail.com>
2013-09-20 15:49:49 -0500

    Adjusting formatting.

--- aeapp/js/app.js
+++ aeapp/js/app.js
@@ -1,7 +1,7 @@
-angular
-    .module('emailGen', ['ui.bootstrap'])
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider
-        .when('/', {templateUrl: 'partials/form.html', controller: MainCtrl})
-        .otherwise({ redirectTo: '/' });
+angular.
+    module('emailGen', ['ui.bootstrap']).
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+        when('/', {templateUrl: 'partials/form.html', controller: MainCtrl}).
+        otherwise({ redirectTo: '/' });
 }]);
