f283972619983246ba13e867ae09f3a4d9eff139
Kevin deLeon <kevin.deleon@gmail.com>
2013-09-20 14:34:06 -0500

    Fixing spacing…again.

--- aeapp/resources/js/assets.js
+++ aeapp/resources/js/assets.js
@@ -1,124 +1,124 @@
 {
-	"buttons" : [
-		{
-			"id" : "apply-button",
-			"alt" : "Apply Now",
-			"fname" : "apply-now.jpg",
-			"url" : "email-assets/buttons/apply-now.jpg"
-		},
-		{
-			"id" : "enroll-button",
-			"alt" : "Enroll Now",
-			"fname" : "enroll-now.jpg",
-			"url" : "email-assets/buttons/enroll-now.jpg"
-		},
-		{
-			"id" : "visit-button",
-			"alt" : "Visit Loyola",
-			"fname" : "visit-loyola.jpg",
-			"url" : "email-assets/buttons/visit-loyola.jpg"
-		}
-	],
-	"header_images" : [
-		{
-			"id" : "havoc-header",
-			"alt" : "Loyola University New Orleans &ndash; Wolfpack",
-			"fname" : "admissions-general-havoc-header.jpg",
-			"url" : "email-assets/header-images/admissions-general-havoc-header.jpg"
-		},
-		{
-			"id" : "cob-header",
-			"alt" : "Loyola University New Orleans",
-			"fname" : "cob-banner.jpg",
-			"url" : "email-assets/header-images/cob-banner.jpg"
-		},
-		{
-			"id" : "css-header",
-			"alt" : "Loyola University New Orleans",
-			"fname" : "css-banner.jpg",
-			"url" : "email-assets/header-images/css-banner.jpg"
-		},
-		{
-			"id" : "huns-header",
-			"alt" : "Loyola University New Orleans",
-			"fname" : "huns-banner.jpg",
-			"url" : "email-assets/header-images/huns-banner.jpg"
-		}
-	],
-	"sidebar_images" : [
-		{
-			"id" : "aasbc-side",
-			"alt" : "AASBC International",
-			"fname" : "aasbc.jpg",
-			"url" : "email-assets/sidebar-images/aasbc.jpg"
-		},
-		{
-			"id" : "cob-academic-side",
-			"alt" : "Academic Programs",
-			"fname" : "cob-academic-programs.jpg",
-			"url" : "email-assets/sidebar-images/cob-academic-programs.jpg"
-		},
-		{
-			"id" : "css-academic-side",
-			"alt" : "Academic Programs",
-			"fname" : "css-academic-programs.jpg",
-			"url" : "email-assets/sidebar-images/css-academic-programs.jpg"
-		},
-		{
-			"id" : "css-centers-side",
-			"alt" : "Centers and Institutes",
-			"fname" : "css-centers-and-institutes.jpg",
-			"url" : "email-assets/sidebar-images/css-centers-and-institutes.jpg"
-		},
-		{
-			"id" : "find-side",
-			"alt" : "Find Your Place at Loyola",
-			"fname" : "find-your-place.jpg",
-			"url" : "email-assets/sidebar-images/find-your-place.jpg"
-		},
-		{
-			"id" : "research-side",
-			"alt" : "Academic Research",
-			"fname" : "huns-academic-research.jpg",
-			"url" : "email-assets/sidebar-images/huns-academic-research.jpg"
-		},
-		{
-			"id" : "journalism-side",
-			"alt" : "Journalism",
-			"fname" : "journalism.jpg",
-			"url" : "email-assets/sidebar-images/journalism.jpg"
-		},
-		{
-			"id" : "pr-side",
-			"alt" : "Public Relations",
-			"fname" : "public-relations.jpg",
-			"url" : "email-assets/sidebar-images/public-relations.jpg"
-		},
-		{
-			"id" : "wpc-side",
-			"alt" : "Walker Percy Center",
-			"fname" : "walker-percy.jpg",
-			"url" : "email-assets/sidebar-images/walker-percy.jpg"
-		}
-	],
-	"video_images" : [
-		{
-			"id" : "financial-vid",
-			"alt" : "Financial Management",
-			"fname" : "cob-video.jpg",
-			"url" : "email-assets/video-images/cob-video.jpg"
-		},
-		{
-			"id" : "culture-vid",
-			"alt" : "Play Now",
-			"fname" : "food-music-culture-video.jpg",
-			"url" : "email-assets/video-images/food-music-culture-video.jpg"
-		},
-		{
-			"id" : "bateman-vid",
-			"alt" : "",
-			"fname" : "loyola-bateman-team.jpg",
-			"url" : "email-assets/video-images/loyola-bateman-team.jpg"
-		}
-	]
+    "buttons" : [
+        {
+            "id" : "apply-button",
+            "alt" : "Apply Now",
+            "fname" : "apply-now.jpg",
+            "url" : "email-assets/buttons/apply-now.jpg"
+        },
+        {
+            "id" : "enroll-button",
+            "alt" : "Enroll Now",
+            "fname" : "enroll-now.jpg",
+            "url" : "email-assets/buttons/enroll-now.jpg"
+        },
+        {
+            "id" : "visit-button",
+            "alt" : "Visit Loyola",
+            "fname" : "visit-loyola.jpg",
+            "url" : "email-assets/buttons/visit-loyola.jpg"
+        }
+    ],
+    "header_images" : [
+        {
+            "id" : "havoc-header",
+            "alt" : "Loyola University New Orleans &ndash; Wolfpack",
+            "fname" : "admissions-general-havoc-header.jpg",
+            "url" : "email-assets/header-images/admissions-general-havoc-header.jpg"
+        },
+        {
+            "id" : "cob-header",
+            "alt" : "Loyola University New Orleans",
+            "fname" : "cob-banner.jpg",
+            "url" : "email-assets/header-images/cob-banner.jpg"
+        },
+        {
+            "id" : "css-header",
+            "alt" : "Loyola University New Orleans",
+            "fname" : "css-banner.jpg",
+            "url" : "email-assets/header-images/css-banner.jpg"
+        },
+        {
+            "id" : "huns-header",
+            "alt" : "Loyola University New Orleans",
+            "fname" : "huns-banner.jpg",
+            "url" : "email-assets/header-images/huns-banner.jpg"
+        }
+    ],
+    "sidebar_images" : [
+        {
+            "id" : "aasbc-side",
+            "alt" : "AASBC International",
+            "fname" : "aasbc.jpg",
+            "url" : "email-assets/sidebar-images/aasbc.jpg"
+        },
+        {
+            "id" : "cob-academic-side",
+            "alt" : "Academic Programs",
+            "fname" : "cob-academic-programs.jpg",
+            "url" : "email-assets/sidebar-images/cob-academic-programs.jpg"
+        },
+        {
+            "id" : "css-academic-side",
+            "alt" : "Academic Programs",
+            "fname" : "css-academic-programs.jpg",
+            "url" : "email-assets/sidebar-images/css-academic-programs.jpg"
+        },
+        {
+            "id" : "css-centers-side",
+            "alt" : "Centers and Institutes",
+            "fname" : "css-centers-and-institutes.jpg",
+            "url" : "email-assets/sidebar-images/css-centers-and-institutes.jpg"
+        },
+        {
+            "id" : "find-side",
+            "alt" : "Find Your Place at Loyola",
+            "fname" : "find-your-place.jpg",
+            "url" : "email-assets/sidebar-images/find-your-place.jpg"
+        },
+        {
+            "id" : "research-side",
+            "alt" : "Academic Research",
+            "fname" : "huns-academic-research.jpg",
+            "url" : "email-assets/sidebar-images/huns-academic-research.jpg"
+        },
+        {
+            "id" : "journalism-side",
+            "alt" : "Journalism",
+            "fname" : "journalism.jpg",
+            "url" : "email-assets/sidebar-images/journalism.jpg"
+        },
+        {
+            "id" : "pr-side",
+            "alt" : "Public Relations",
+            "fname" : "public-relations.jpg",
+            "url" : "email-assets/sidebar-images/public-relations.jpg"
+        },
+        {
+            "id" : "wpc-side",
+            "alt" : "Walker Percy Center",
+            "fname" : "walker-percy.jpg",
+            "url" : "email-assets/sidebar-images/walker-percy.jpg"
+        }
+    ],
+    "video_images" : [
+        {
+            "id" : "financial-vid",
+            "alt" : "Financial Management",
+            "fname" : "cob-video.jpg",
+            "url" : "email-assets/video-images/cob-video.jpg"
+        },
+        {
+            "id" : "culture-vid",
+            "alt" : "Play Now",
+            "fname" : "food-music-culture-video.jpg",
+            "url" : "email-assets/video-images/food-music-culture-video.jpg"
+        },
+        {
+            "id" : "bateman-vid",
+            "alt" : "",
+            "fname" : "loyola-bateman-team.jpg",
+            "url" : "email-assets/video-images/loyola-bateman-team.jpg"
+        }
+    ]
 }
--- aeapp/resources/js/main.js
+++ aeapp/resources/js/main.js
@@ -1,23 +1,23 @@
 angular.module('main', ['ui.bootstrap']);
 
 function AccordionDemoCtrl($scope) {
-	$scope.oneAtATime = true;
+    $scope.oneAtATime = true;
 
-	$scope.groups = [
-	{
-		title: "Dynamic Group Header - 1",
-		content: "Dynamic Group Body - 1"
-	},
-	{
-		title: "Dynamic Group Header - 2",
-		content: "Dynamic Group Body - 2"
-	}
-	];
+    $scope.groups = [
+    {
+        title: "Dynamic Group Header - 1",
+        content: "Dynamic Group Body - 1"
+    },
+    {
+        title: "Dynamic Group Header - 2",
+        content: "Dynamic Group Body - 2"
+    }
+    ];
 
-	$scope.items = ['Item 1', 'Item 2', 'Item 3'];
+    $scope.items = ['Item 1', 'Item 2', 'Item 3'];
 
-	$scope.addItem = function() {
-		var newItemNo = $scope.items.length + 1;
-		$scope.items.push('Item ' + newItemNo);
-	};
+    $scope.addItem = function() {
+        var newItemNo = $scope.items.length + 1;
+        $scope.items.push('Item ' + newItemNo);
+    };
 }
