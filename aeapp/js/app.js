26a6dfb3c7662568d312857db14d22be8c18e079
Kevin deLeon <kevin.deleon@gmail.com>
2013-09-21 13:18:24 -0500

    Nav controller, About controller and active classes added.

--- aeapp/js/app.js
+++ aeapp/js/app.js
@@ -2,6 +2,7 @@ angular.
     module('emailGen', ['ui.bootstrap']).
     config(['$routeProvider', function($routeProvider) {
         $routeProvider.
-        when('/', {templateUrl: 'partials/form.html', controller: MainCtrl}).
+        when('/', { templateUrl: 'partials/form.html', controller: MainCtrl }).
+        when('/about', { templateUrl: 'partials/about.html', controller: AboutCtrl }).
         otherwise({ redirectTo: '/' });
 }]);
--- aeapp/js/controllers.js
+++ aeapp/js/controllers.js
@@ -1,3 +1,15 @@
 function MainCtrl($scope, $routeParams) {
-    $scope.test = "test";
+    $scope.test = "index test";
+}
+
+// Controls header/navigation actions, classes, etc...
+function HeaderCtrl ($scope, $location) {
+    $scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+}
+
+// Loads any particulars into the About view
+function AboutCtrl($scope, $routeParams) {
+    $scope.test = "about test";
 }
