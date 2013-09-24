fbe267a10f8aadb5f01799cad9e34373d1ec0f17
Kevin deLeon <kevin.deleon@gmail.com>
2013-09-24 14:57:14 -0500

    setting up form and grabbing data from assets in controllers.js

--- aeapp/js/controllers.js
+++ aeapp/js/controllers.js
@@ -1,15 +1,20 @@
-function MainCtrl($scope, $routeParams) {
-    $scope.test = "index test";
+function MainCtrl($scope, $http) {
+    $http.get('js/assets.json').then(function(res){
+        $scope.assets = res.data;
+        // set default banner as first in array
+        $scope.headerimage = $scope.assets.header_images[0].url;               
+    });
 }
 
 // Controls header/navigation actions, classes, etc...
 function HeaderCtrl ($scope, $location) {
+    //function to check which page is active.
     $scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
     };
 }
 
 // Loads any particulars into the About view
-function AboutCtrl($scope, $routeParams) {
+function AboutCtrl($scope) {
     $scope.test = "about test";
 }
