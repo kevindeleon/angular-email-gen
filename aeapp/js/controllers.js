473c76804e77ea3c4e2fece2088605a236e1357a
Kevin deLeon <kevin.deleon@gmail.com>
2013-09-26 09:56:59 -0500

    Adding some comments for documentation

--- aeapp/js/controllers.js
+++ aeapp/js/controllers.js
@@ -1,22 +1,23 @@
 function MainCtrl($scope, $http) {
+    // retrieve data from json file
     $http.get('js/assets.json').then(function(res){
         $scope.assets = res.data;
         // set default banner as first in array and also default alt
         $scope.assets.header_image = $scope.assets.header_images[0].url;
         $scope.assets.header_image_alt = $scope.assets.header_images[0].alt;
 
-        // set default video image as first in array
+        // set default video image as first in array and also default alt
         $scope.assets.video_image = $scope.assets.video_images[0].url;
         $scope.assets.video_image_alt = $scope.assets.video_images[0].alt;
 
-        // set default button image as first in array
+        // set default button image as first in array and also default alt
         $scope.assets.button_image = $scope.assets.buttons[0].url;
         $scope.assets.button_image_alt = $scope.assets.buttons[0].alt;
     });
 }
 
 // Controls header/navigation actions, classes, etc...
-function HeaderCtrl ($scope, $location) {
+function NavCtrl ($scope, $location) {
     //function to check which page is active.
     $scope.isActive = function (viewLocation) { 
         return viewLocation === $location.path();
