d8f02286fa2f36824b21565c5c179b46032422c8
Kevin deLeon <kevin.deleon@gmail.com>
2013-09-26 09:46:29 -0500

    Re-naming variables for consistency and clarity, adding alt values when a radio is clicked

--- aeapp/js/controllers.js
+++ aeapp/js/controllers.js
@@ -2,14 +2,16 @@ function MainCtrl($scope, $http) {
     $http.get('js/assets.json').then(function(res){
         $scope.assets = res.data;
         // set default banner as first in array and also default alt
-        $scope.assets.headerimage = $scope.assets.header_images[0].url;
-        $scope.assets.headeralt = $scope.assets.header_images[0].alt;
+        $scope.assets.header_image = $scope.assets.header_images[0].url;
+        $scope.assets.header_image_alt = $scope.assets.header_images[0].alt;
 
         // set default video image as first in array
-        $scope.assets.videoimage = $scope.assets.video_images[0].url;
+        $scope.assets.video_image = $scope.assets.video_images[0].url;
+        $scope.assets.video_image_alt = $scope.assets.video_images[0].alt;
 
         // set default button image as first in array
-        $scope.assets.buttonimage = $scope.assets.buttons[0].url;
+        $scope.assets.button_image = $scope.assets.buttons[0].url;
+        $scope.assets.button_image_alt = $scope.assets.buttons[0].alt;
     });
 }
 
